9169bb783318369048fd305fcc9dfcf70c750a6a
Abdul Kader <abdul.kader880@gmail.com>
2019-07-27 16:52:22 -0500

    updated stock summary

--- pynasdaq/stock.py
+++ pynasdaq/stock.py
@@ -25,31 +25,22 @@ def currentPrice(symbol):
 
 
 def stockSummaryQuote(symbol):
+    '''
+    Arg: A symbol
+    Returns: DataFrame with summary quote. All field type string 
+    '''
     response = requests.get(STOCK_SUMMARY_QUOTE_URL.format(symbol=symbol))
     docTree = html.fromstring(response.content)
 
-    curPrice = float(docTree.xpath(
-        '//div[@id="qwidget_lastsale"]/text()')[0].strip()[1:])
+    curPrice = docTree.xpath('(//div[@id="qwidget_lastsale"])[1]/text()')[0].strip()
 
-    colspans = docTree.xpath(
-        '//div[@class="row overview-results relativeP"]')
-    tableRows = colspans[0].xpath('.//div[@class="table-row"]')
+    tableRows = docTree.xpath('(.//div[@class="row overview-results relativeP"])[1]//div[@class="table-row"]')
 
     data = {"Symbol": symbol, "CurrentPrice": curPrice}
     for row in tableRows:
         cells = row.xpath('./div[@class="table-cell"]')
         key = cells[0].xpath('./b/text()')[0].strip()
-        valStr = cells[1].text.strip()
-        if key == "Previous Close" or key == "Annualized Dividend" or key == "Earnings Per Share (EPS)":
-            val = float(cells[1].text.strip()[1:])
-        elif key == "1 Year Target" or key == "P/E Ratio" or key == "Forward P/E (1y)" or key == "Beta":
-
-            val = 0 if valStr == "" else float(valStr)
-
-        elif key == "Market Cap" or key == "Share Volume" or key == "50 Day Avg. Daily Volume":
-            val = int(cells[1].text.strip().replace(",", ""))
-        else:
-            val = cells[1].text.strip()
+        val = cells[1].text.strip()
         data[key] = val
 
     return pd.DataFrame(data, index=[0])
