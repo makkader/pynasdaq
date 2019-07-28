c6c2b8f614e4c9838fe3c6bbf81da2cc1b1198d3
Abdul Kader <abdul.kader880@gmail.com>
2019-07-28 13:08:07 -0500

    updated usages and version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pynasdaq',
-      version='0.1.1',
+      version='0.2.0',
       description='Retrieve NASDAQ stock and dividend data',
       url='https://github.com/makkader/pynasdaq',
       author='Mak Kader',
